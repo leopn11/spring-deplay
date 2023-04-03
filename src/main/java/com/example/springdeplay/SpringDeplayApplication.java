abcbae2502074d14ed58c713c6e11e9512571c8a
Leonardo Pinto <leo071729@gmail.com>
2023-04-03 09:14:40 -0500

    spring heroku app config

---
+++ src/main/java/com/example/springdeplay/SpringDeplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdeplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdeplay/SpringDeplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdeplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
